76c92d5845bfc0e518d568df643e5e99dfd731df
Noldia Chavez <ivar.n.c.29@gmail.com>
2025-06-05 23:09:36 -0500

    feat: Configuracion inicial

---
+++ src/main/java/pe/edu/utp/visit_secure/VisitSecureApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.visit_secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/utp/visit_secure/VisitSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.visit_secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
